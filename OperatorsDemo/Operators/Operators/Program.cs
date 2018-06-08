b02fc3756d3c9bee68141abc85d2f6f168ef52fa
surekhasrinivasan <surekhasrao@gmail.com>
2018-06-08 13:42:03 -0400

    Operators demo

---
+++ OperatorsDemo/Operators/Operators/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 10;
+            var b = 3;
+
+            Console.WriteLine(a + b);
+
+            //returns integer value 
+            Console.WriteLine(a / b);
+
+            //returns float value 
+            Console.WriteLine((float)a / (float)b);
+        }
+    }
+}
