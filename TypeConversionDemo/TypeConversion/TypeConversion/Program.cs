1199c4b41db8b62bb4e2cb4ffb5753e5db2ea5fc
surekhasrinivasan <surekhasrao@gmail.com>
2018-06-08 13:20:11 -0400

    Type conversion demo

--- TypeConversionDemo/TypeConversion/TypeConversion/Program.cs
+++ TypeConversionDemo/TypeConversion/TypeConversion/Program.cs
@@ -20,10 +20,35 @@ namespace TypeConversion
             //byte b = (byte)i;
             //Console.WriteLine(b);
 
-            //Incompatible types
-            var number = "1234";
-            int i = Convert.ToInt32(number);
-            Console.WriteLine(i);
+            //Incompatible types string to integer so we use Convert
+            //var number = "1234";
+            //int i = Convert.ToInt32(number);
+            //Console.WriteLine(i);
+
+            //coverting string to byte 
+            //try
+            //{
+                //var number = "1234";
+                //byte b = Convert.ToByte(number);
+                //Console.WriteLine(b);
+            //}
+            //catch (Exception)
+            //{
+                //Console.WriteLine("The number could not be converted to a byte");
+            //}
+
+            //coverting string to boolean 
+            try
+            {
+                string str = "true";
+                bool b = Convert.ToBoolean(str);
+                Console.WriteLine(b);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("The str could not be converted to a boolean");
+            }
+
         }
     }
 }
