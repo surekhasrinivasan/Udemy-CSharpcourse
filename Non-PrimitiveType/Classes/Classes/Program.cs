591a644d6eb407fffe503ba1fb38d09beb8a29db
surekhasrinivasan <surekhasrao@gmail.com>
2018-06-11 10:35:45 -0400

    Non-Primitive Type - Classes Demo

---
+++ Non-PrimitiveType/Classes/Classes/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var john = new Person();
+            john.FirstName = "John";
+            john.LastName = "Smith";
+            john.Introduce();
+        }
+    }
+}
