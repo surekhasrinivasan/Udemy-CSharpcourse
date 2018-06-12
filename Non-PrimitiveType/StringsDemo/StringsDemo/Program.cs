e2bd71ab4eb0f8b5cc84334f40922dfa377d2fb2
surekhasrinivasan <surekhasrao@gmail.com>
2018-06-11 22:00:14 -0400

    Strings Demo

---
+++ Non-PrimitiveType/StringsDemo/StringsDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstName = "Surekha";
+            var lastName = "Srinivasan";
+
+            var fullName = firstName + " " + lastName;
+            //Console.WriteLine(fullName);
+            
+            var myFullName = string.Format("My name is {0}, {1}", firstName, lastName);
+            //Console.WriteLine(myFullName);
+
+            var names = new string[3] { "John", "Jack", "Mary" };
+            var formattedNames = string.Join(", ", names);
+
+            //Console.WriteLine(formattedNames);
+
+            var text = @"Hi John
+Look into the following paths
+C:\folder1\folder2
+C:\folder3\folder4";
+Console.WriteLine(text);
+        }
+    }
+}
