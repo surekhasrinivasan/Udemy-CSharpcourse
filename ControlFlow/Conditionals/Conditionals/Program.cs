3d2fa1b3514b8b9876b40e043e1e512c40d58e17
surekhasrinivasan <surekhasrao@gmail.com>
2018-06-15 14:49:36 -0400

    Demo of if/else conditionals

---
+++ ControlFlow/Conditionals/Conditionals/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditionals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Example for if/else statements
+            int hour = 10;
+
+            if (hour > 0 && hour < 12)
+            {
+                Console.WriteLine("It's morning.");
+            }
+            else if (hour >=12 && hour < 18)
+            {
+                Console.WriteLine("It's afternoon.");
+            }
+            else
+            {
+                Console.WriteLine("It's evening");
+            }
+        }
+    }
+}
