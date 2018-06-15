4bdffbb66f80594276f0aebf968e92f2f1a8631e
surekhasrinivasan <surekhasrao@gmail.com>
2018-06-15 15:20:37 -0400

    Demo for SwitchCase

--- ControlFlow/Conditionals/Conditionals/Program.cs
+++ ControlFlow/Conditionals/Conditionals/Program.cs
@@ -10,34 +10,22 @@ namespace Conditionals
     {
         static void Main(string[] args)
         {
-            //Example for if/else statements
-            /*int hour = 10;
+            var season = Season.Autumn;
 
-            if (hour > 0 && hour < 12)
+            switch (season)
             {
-                Console.WriteLine("It's morning.");
+                case Season.Autumn:
+                    Console.WriteLine("It's autumn and a beautiful season.");
+                    break;
+
+                case Season.Summer:
+                    Console.WriteLine("It's perfect to go to beach");
+                    break;
+
+                default:
+                    Console.WriteLine("I don't understand that season!");
+                    break;
             }
-            else if (hour >=12 && hour < 18)
-            {
-                Console.WriteLine("It's afternoon.");
-            }
-            else
-            {
-                Console.WriteLine("It's evening");
-            }*/
-
-            //Example using conditional operator
-            bool isGoldCustomer = true;
-
-            //float price;
-            //if(isGoldCustomer)
-            //price = 19.95f;
-            //else
-            //price = 29.95f;
-
-            float price = (isGoldCustomer) ? 19.95f : 29.95f;
-
-            Console.WriteLine(price);
         }
     }
 }
