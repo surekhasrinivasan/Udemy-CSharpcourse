1f8ed83e8308e34b92359809846c6962508e0352
surekhasrinivasan <surekhasrao@gmail.com>
2018-06-16 06:21:23 -0400

    ForEach Loop Demo

---
+++ Loops/ForEachDemo/ForEachDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForEachDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var name = "Surekha Srinivasan";
+
+            //for (var i = 0; i < name.Length; i++)
+            //{
+            //    Console.WriteLine(name[i]);
+            //}
+
+            //foreach for string
+            //foreach(var character in name)
+            //{
+            //    Console.WriteLine(character);
+            //}
+
+            //foreach for numbers
+            var numbers = new int[] { 1, 2, 3, 4 };
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
