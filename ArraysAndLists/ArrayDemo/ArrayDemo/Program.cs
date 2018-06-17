e058136deca047e9a5c605ebe8713297cf9184da
surekhasrinivasan <surekhasrao@gmail.com>
2018-06-17 17:05:53 -0400

    Array Demo

---
+++ ArraysAndLists/ArrayDemo/ArrayDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new[] { 3, 7, 9, 2, 14, 6 };
+
+            //Length
+            Console.WriteLine("Length: " + numbers.Length);
+
+            //IndexOf()
+            var index = Array.IndexOf(numbers, 9);
+
+            Console.WriteLine("Index of 9: " + index);
+
+            //Clear()
+            Array.Clear(numbers, 0, 2);
+
+            Console.WriteLine("Effect of Clear()");
+            foreach (var n in numbers)
+                Console.WriteLine(n);
+
+            //Copy()
+            int[] another = new int[4];
+            Array.Copy(numbers, another, 4);
+
+            Console.WriteLine("Effect of Copy()");
+            foreach (var n in another)
+                Console.WriteLine(n);
+        }
+    }
+}
