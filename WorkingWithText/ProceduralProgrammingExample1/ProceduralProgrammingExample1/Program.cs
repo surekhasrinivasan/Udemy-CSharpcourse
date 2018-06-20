3dcbf902bc717c80f7f962ebd34dac5fa9d2a249
surekhasrinivasan <surekhasrao@gmail.com>
2018-06-20 18:02:45 -0400

    Procedural Programming Example-1 Reverse Name

---
+++ WorkingWithText/ProceduralProgrammingExample1/ProceduralProgrammingExample1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProceduralProgrammingExample1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's your name? ");
+            var name = Console.ReadLine();
+            var reversed = ReverseName(name);
+            Console.WriteLine("Reversed Name: " + reversed);
+        }
+
+        public static string ReverseName(string name)
+        {
+            var array = new char[name.Length];
+            for (var i = name.Length; i > 0; i--)
+               array[name.Length - i] = name[i - 1];
+
+                return new string(array);
+        }
+    }
+}
