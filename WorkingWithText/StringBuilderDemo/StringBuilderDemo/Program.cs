ceac8af76d99ecc767946d3a8fe8873e8488af85
surekhasrinivasan <surekhasrao@gmail.com>
2018-06-20 17:25:22 -0400

    StringBuilder Demo

---
+++ WorkingWithText/StringBuilderDemo/StringBuilderDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringBuilderDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new StringBuilder("Hello World");
+
+            builder
+                .Append('-', 10)
+                .AppendLine()
+                .Append("Header")
+                .AppendLine()
+                .Append('-', 10)
+                .Replace('-', '+')
+                .Remove(0, 10);
+
+            builder.Insert(0, new string('-', 10));
+
+            Console.WriteLine(builder);
+
+            Console.WriteLine("First Character: " + builder[0]);
+        }
+    }
+}
