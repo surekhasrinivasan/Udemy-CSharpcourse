c54de5f3bb7972f99ef9aca07dce31a3bf6e2048
surekhasrinivasan <surekhasrao@gmail.com>
2018-06-20 18:19:55 -0400

    Procedural Programming Example-2 Display Unique Numbers

---
+++ WorkingWithText/ProceduralProgrammingExample2/ProceduralProgrammingExample2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProceduralProgrammingExample2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>();
+
+            while (true)
+            {
+                Console.Write("Enter a number (or 'Quit' to exit): ");
+                var input = Console.ReadLine();
+
+                if (input.ToLower() == "quit")
+                    break;
+
+                numbers.Add(Convert.ToInt32(input));
+            }
+            Console.WriteLine("Unique Numbers: ");
+            foreach (var number in GetUniqueNumbers(numbers))
+                Console.WriteLine(number);
+        }
+
+        public static List<int> GetUniqueNumbers(List<int> numbers)
+        {
+            var uniques = new List<int>();
+            foreach (var number in numbers)
+            {
+                if (!uniques.Contains(number))
+                    uniques.Add(number);
+            }
+            return uniques;
+        }
+    }
+}
