27b68a58aebda490c17f8afe69a8a5e699e2a260
surekhasrinivasan <surekhasrao@gmail.com>
2018-06-20 17:03:34 -0400

    Summarizing Text demo

---
+++ WorkingWithText/SummarizingText/SummarizingText/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SummarizingText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sentence = "This is going to be a really really really really really long sentence.";
+            var summary = StringUtility.SummarizeText(sentence, 30);
+            Console.WriteLine(summary);
+        }
+    }
+}
