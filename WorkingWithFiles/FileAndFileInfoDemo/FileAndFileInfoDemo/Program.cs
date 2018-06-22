74d1451371ff6f46b5fbac3b460330005907d0bb
surekhasrinivasan <surekhasrao@gmail.com>
2018-06-21 22:01:49 -0400

    File and FileInfo Demo

---
+++ WorkingWithFiles/FileAndFileInfoDemo/FileAndFileInfoDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace FileAndFileInfoDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+            var path = @"c:\somefile.jpg";
+
+            File.Copy(@"c:\temp\myfile.jpg", @"d:\temp\myfile.jpg", true);
+            File.Delete(path);
+            if (File.Exists(path))
+            {
+                //
+            }
+            var content = File.ReadAllText(path);
+        
+            var fileInfo = new FileInfo(path);
+
+            fileInfo.CopyTo("....");
+            fileInfo.Delete();
+            if (FileInfo.Exists(path))
+            {
+                //
+            }
+    }
+}
